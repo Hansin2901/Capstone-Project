8b01fa4e2ecd62446ca4de9802f2083fb87813c1
Hansin2901 <hansintpatwa@gmail.com>
2023-06-06 16:18:03 +0530

    First Addition of all the files

---
+++ Hungarian/Base/Node.java
@@ -0,0 +1,21 @@
+package Base;
+class Node   
+{  
+// int  y,shortestDist;
+boolean setB;  
+int value, weight;  
+Node(int value, int weight)   
+{  
+this.value = value;  
+this.weight = weight;  
+}  
+//creating a constructor of the class Vertex  
+// Node(boolean set)   
+// {  
+// this.y = 0;  
+// setB=set;
+// }  
+//overrides the toString() method  
+ 
+}  
+
---
+++ Hungarian/Base/Vertex.java
@@ -0,0 +1,11 @@
+package Base;
+
+public class Vertex {
+    int  y,shortestDist;
+    boolean matched,setB;
+    Vertex()   
+    {  
+    this.y = 0;  
+    // setB=set;
+    }  
+}
